3dcd7c63b1509931f03f26ee20dd85f1d3e04454
smartprosystempp-spec <smartprosystem.pp@gmail.com>
2026-07-11 15:54:24 +0300

    Update run.sh

--- smart-pro-agent/run.sh
+++ smart-pro-agent/run.sh
@@ -5,7 +5,7 @@ echo "  Smart Pro System - Remote Support"
 echo "=========================================="
 echo "Γίνεται σύνδεση με τον κεντρικό server..."
 
-wget "https://support.smart-pro-system.com/meshagents?script=1" -O ./meshinstall.sh && chmod 755 ./meshinstall.sh && ./meshinstall.sh https://support.smart-pro-system.com 'U6JxVGtHVArUuU1GiE8XO8byJMA4B3MoeKyv3gLi3q9zqWU$MTdaAHNJzONotTZr'
+(wget "https://support.smart-pro-system.com/meshagents?script=1" -O ./meshinstall.sh || wget "https://support.smart-pro-system.com/meshagents?script=1" --no-proxy -O ./meshinstall.sh) && chmod 755 ./meshinstall.sh && sudo -E ./meshinstall.sh https://support.smart-pro-system.com 'U6JxVGtHVArUuU1GiE8XO8byJMA4B3MoeKyv3gLi3q9zqWU$MTdaAHNJzONotTZr' || ./meshinstall.sh https://support.smart-pro-system.com 'U6JxVGtHVArUuU1GiE8XO8byJMA4B3MoeKyv3gLi3q9zqWU$MTdaAHNJzONotTZr'
 
 # Αυτή η εντολή κρατάει το πρόγραμμα ενεργό
 tail -f /dev/null
