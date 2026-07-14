274cc38f2410822a429b9a71c44763e9b3b21864
smartprosystempp-spec <smartprosystem.pp@gmail.com>
2026-07-14 12:28:02 +0300

    Update run.sh

--- smart-pro-agent/run.sh
+++ smart-pro-agent/run.sh
@@ -3,16 +3,21 @@
 echo "=========================================="
 echo "  Smart Pro System - Remote Support"
 echo "=========================================="
+echo "Προετοιμασία μόνιμου χώρου αποθήκευσης..."
+
+# Μεταφερόμαστε στον μόνιμο φάκελο /data που δεν σβήνεται ποτέ στις επανεκκινήσεις
+cd /data
+
 echo "Γίνεται απευθείας λήψη και εκκίνηση του πράκτορα..."
 
-# 1. Λήψη του εκτελέσιμου αρχείου
-wget -q "https://support.smart-pro-system.com/meshagents?id=26" -O /meshagent
+# 1. Λήψη του εκτελέσιμου αρχείου στον φάκελο /data
+wget -q "https://support.smart-pro-system.com/meshagents?id=26" -O /data/meshagent
 
-# 2. Λήψη του αρχείου ρυθμίσεων (προσοχή στα μονά εισαγωγικά, είναι κρίσιμα!)
-wget -q 'https://support.smart-pro-system.com/meshsettings?id=U6JxVGtHVArUuU1GiE8XO8byJMA4B3MoeKyv3gLi3q9zqWU$MTdaAHNJzONotTZr' -O /meshagent.msh
+# 2. Λήψη του αρχείου ρυθμίσεων στον φάκελο /data
+wget -q 'https://support.smart-pro-system.com/meshsettings?id=U6JxVGtHVArUuU1GiE8XO8byJMA4B3MoeKyv3gLi3q9zqWU$MTdaAHNJzONotTZr' -O /data/meshagent.msh
 
 # 3. Δίνουμε δικαιώματα εκτέλεσης
-chmod +x /meshagent
+chmod +x /data/meshagent
 
-# 4. Εκτέλεση του προγράμματος στο προσκήνιο
-exec /meshagent
+# 4. Εκτέλεση του προγράμματος από τον μόνιμο φάκελο
+exec /data/meshagent
